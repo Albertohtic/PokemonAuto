d3f39c31f5f849ff9864966aae5f2cfc7057a070
Albertohtic <167212352+Albertohtic@users.noreply.github.com>
2025-02-13 08:19:52 +0100

    Update PokemonService.java

--- src/main/java/api/PokemonService.java
+++ src/main/java/api/PokemonService.java
@@ -4,24 +4,20 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 public class PokemonService {
-    // Base URI del API
     private static final String BASE_URI = "https://pokeapi.co";
 
     static {
         RestAssured.baseURI = BASE_URI;
     }
 
-    // Endpoint: All Pokemon
     public Response getAllPokemon() {
         return RestAssured.given().when().get("/api/v2/pokemon/");
     }
 
-    // Endpoint: Pokemon Info (por id o nombre)
     public Response getPokemonInfo(String idOrName) {
         return RestAssured.given().when().get("/api/v2/pokemon/" + idOrName);
     }
 
-    // Endpoint: Pokemon Abilities (por id o nombre)
     public Response getPokemonAbility(String idOrName) {
         return RestAssured.given().when().get("/api/v2/ability/" + idOrName);
     }
